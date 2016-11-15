d421a870af96d192d55579a6e56b21ef567dee3e
Francisco A. Lozano <flozano@gmail.com>
2016-11-15 02:51:41 +0100

    Let spring boot manage the mockito dependency

--- src/test/java/com/flozano/s3mavenproxy/web/IntegrationWebTest.java
+++ src/test/java/com/flozano/s3mavenproxy/web/IntegrationWebTest.java
@@ -8,14 +8,16 @@ import java.io.InputStream;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
-import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.support.BasicAuthorizationInterceptor;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestOperations;
 
@@ -23,36 +25,34 @@ import com.flozano.s3mavenproxy.Integration;
 import com.flozano.s3mavenproxy.config.Config;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Config.class)
-@WebIntegrationTest(randomPort = true)
+@SpringBootTest(classes = Config.class, webEnvironment = WebEnvironment.RANDOM_PORT)
 @Category(Integration.class)
 public class IntegrationWebTest {
 
 	@Value("${local.server.port}")
 	int port;
-
-	RestOperations restOperations = new TestRestTemplate("test", "test");
+	@Autowired
+	RestTemplateBuilder restTemplateBuilder;
+	RestOperations restOperations;
 
 	String path = "/com/flozano/s3mavenproxy/dummy-artifact-1.0.txt";
 
 	@Test
 	public void test() {
+		restOperations = restTemplateBuilder.additionalInterceptors(new BasicAuthorizationInterceptor("test", "test"))
+				.build();
 		String url = "http://127.0.0.1:" + port + path;
-		ResponseEntity<Void> putResult = restOperations.exchange(url,
-				HttpMethod.PUT, new HttpEntity<InputStream>(resource()),
+		ResponseEntity<Void> putResult = restOperations.exchange(url, HttpMethod.PUT, new HttpEntity<>(resource()),
 				Void.class);
 		assertEquals(HttpStatus.CREATED, putResult.getStatusCode());
-		ResponseEntity<Void> getResult = restOperations.getForEntity(url,
-				Void.class);
+		ResponseEntity<Void> getResult = restOperations.getForEntity(url, Void.class);
 		assertEquals(HttpStatus.FOUND, getResult.getStatusCode());
 
-		ResponseEntity<Void> getResult2 = restOperations.getForEntity(url,
-				Void.class);
+		ResponseEntity<Void> getResult2 = restOperations.getForEntity(url, Void.class);
 		assertEquals(HttpStatus.FOUND, getResult2.getStatusCode());
 
 		assertNotNull(getResult.getHeaders().get("location"));
-		assertEquals(getResult.getHeaders().get("location"), getResult2
-				.getHeaders().get("location"));
+		assertEquals(getResult.getHeaders().get("location"), getResult2.getHeaders().get("location"));
 	}
 
 	private InputStream resource() {
